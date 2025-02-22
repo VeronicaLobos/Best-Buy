bb87ab2f4ac684e467643e7d65dd6017adf37d4a
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-02-22 12:03:49 +0100

    User interface implemented 2. Show total amount in store

--- main.py
+++ main.py
@@ -3,18 +3,53 @@ from products import Product
 from store import Store
 
 
-# setup initial stock of inventory
-product_list = [ Product(name="MacBook Air M2", price=1450, quantity=100),
-                 Product(name="Bose QuietComfort Earbuds", price=250, quantity=500),
-                 Product(name="Google Pixel 7", price=500, quantity=250)
-               ]
-best_buy = Store(product_list)
-
-# user interface will go here
-
-def _show_all_products_store(my_store):
+# setup initial stock of inventory ---------------------------
+def setup_inventory():
     """
+    Creates and returns a store object:
+    a list containing product objects.
+    """
+    product_list = [ Product(name="MacBook Air M2",
+                             price=1450, quantity=100),
+                 Product(name="Bose QuietComfort Earbuds",
+                         price=250, quantity=500),
+                 Product(name="Google Pixel 7",
+                         price=500, quantity=250)
+               ]
+    return Store(product_list)
 
+# commands available through the user interface go here ------
+
+def __print_total_products(my_store):
+    """
+    Command from the command dispatcher function:
+    2. Show total amount in store
+
+    Gets and iterates though every product object stored
+    in the store object received as an argument, retrieves
+    their quantity attribute and sums them.
+    Prints the total amount formated as a string.
+    """
+    total_amount = 0
+    for product in my_store.get_all_products():
+        total_amount += product.get_quantity()
+
+    print(f"Total of {total_amount} items in store")
+
+
+def __print_all_products_store(my_store):
+    """
+    Command from the command dispatcher function:
+    1. List all products in store
+
+    Arg: my_store is a store object which contains a list of
+        product objects. It has a method to return said
+        product objects, and the product objects have a method
+        that serializes their attributes into a string.
+
+    Retrieves all product objects from the given store,
+    formats each product's information into a string.
+    Prints these strings in an ordered list.
     """
     products = my_store.get_all_products()
 
@@ -24,29 +59,40 @@ def _show_all_products_store(my_store):
         print(f"{ordered_list}. {product.show()}")
         ordered_list += 1
     print("------")
-    print("")
 
 
+# commands for the user interface go here --------------------
 def _command_dispatcher(user_input, my_store):
     """
+    Utility command for the start function.
+
+
+    Args: user_input, the key to access a value in the dict.
+        my_store, given as a parameter for a function call.
     Pairs the menu options (key) with the functions
     available to the user in the CLI program (value).
-    Returns a function call.
-    ...
+    Returns a function call with a store object as
+    parameter.
     """
     commands = {
-        1: _show_all_products_store,
-        2: "hi",
+        1: __print_all_products_store, # tested
+        2: __print_total_products,
         3: "hi",
         4: sys.exit
         }
     return commands[user_input](my_store)
 
+
 def _check_input():
     """
-    Asks user for input, a number between 0 and 10
-    Else, will continue asking for a valid input
-    Returns an integer
+    Utility command for the start function.
+
+
+    Asks user for input, a number between 1 and 4
+    Else, will continue asking for a valid input.
+    Handles errors if wrong value input or number
+    out of range.
+    Returns an integer.
     """
     while True:
         try:
@@ -62,7 +108,11 @@ def _check_input():
 
 def _print_menu():
     """
-    Prints a menu for the user/buyer.
+    Utility command for the start function.
+
+    Prints a menu for the buyer with the
+    command options available from the user
+    interface.
     """
     print("\tStore Menu\n\t----------")
     print("1. List all products in store")
@@ -71,20 +121,32 @@ def _print_menu():
     print("4. Quit")
 
 
-def start(my_store):
+def start(store):
     """
+    Starts the self-service terminal.
+    Allows users to check products, inventory,
+    make and order or exit the terminal.
+
     Gets the store object as a parameter,
     shows the user the menu.
     Checks user input.
-
+    Calls a function chosen by the user.
+    Prints a space after a command has
+    successfully run and restarts.
     """
     while True:
         _print_menu()
         user_input = _check_input()
-        _command_dispatcher(user_input, best_buy)
+        _command_dispatcher(user_input, store)
+        print("")
 
 
 def main():
+    """
+    Opens a store, sets inventory.
+    Starts a self-service terminal.
+    """
+    best_buy = setup_inventory()
     start(best_buy)
 
 
